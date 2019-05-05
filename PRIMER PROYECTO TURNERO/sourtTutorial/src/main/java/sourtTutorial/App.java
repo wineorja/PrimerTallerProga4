f9b12e84bc2c858e6b68d2d8b0c2618ac84ed77c
willingtonortiz <w.ortiz@utp.edu.co>
2019-05-05 01:36:55 -0500

    proyecto Turnero

---
+++ PRIMER PROYECTO TURNERO/sourtTutorial/src/main/java/sourtTutorial/App.java
@@ -0,0 +1,25 @@
+package sourtTutorial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       // System.out.println( "Hello World!" );
+    	
+    		String[] actualNames = new String[]{
+    		"Johnson", "Wilson",
+    		"Wilkinson", "Abraham", "Dagobert"
+    		};
+    		final Sort sorter = new Sort();
+    		sorter.sort(actualNames);
+    		for (final String name : actualNames) {
+    		System.out.println(name);
+    		}
+    		}
+    		
+    }
+
