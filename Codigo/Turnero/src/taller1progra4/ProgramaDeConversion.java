094728be966b12dd77dc3564cd11e7bb0fa5baa7
willingtonortiz <w.ortiz@utp.edu.co>
2019-06-02 18:21:51 -0500

    corrigiendo errores conversor

---
+++ Codigo/Turnero/src/taller1progra4/ProgramaDeConversion.java
@@ -0,0 +1,29 @@
+package taller1progra4;
+
+import controladorEjemplo.ControlConversor;
+import modelo.ConversorEurosPesetas;
+import vistaEjemplo.InterfazVista;
+import vistaEjemplo.VentanaConversor;
+
+public class ProgramaDeConversion {
+	public static void main(String[] args) {
+		//el modelo
+		ConversorEurosPesetas modelo = new ConversorEurosPesetas();
+		
+		// la vista:
+		  InterfazVista<Object> vista = new VentanaConversor();
+		
+		
+		// y el control:
+		 ControlConversor control = new ControlConversor(vista, modelo);
+		 
+		 // configura la vista:
+		 vista.getControlador(control);
+		 
+		 // y arranca la interfaz vista:
+		 vista.arranca();
+		 
+	}
+	
+
+}
